84d27e52b09abe1e9888ba84781024487315f6cc
Meaghan Jones <Guest@epicodus-90.local>
2016-08-19 08:46:14 -0700

    add questions.json seed data file and app now has the functionality to show a question, it's picture and the person who asked the question on the front page

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,16 +3,6 @@ import moment from 'moment';
 
 export default Ember.Route.extend({
   model(){
-    return Ember.RSVP.hash({
-      questions: this.store.findAll('question'),
-      answers: this.store.findAll('answer')
-    });
-  },
-
-  actions: {
-    destroyQuestion(question) {
-      question.destroyRecord();
-      this.transistionTo('index');
-    }
-  }
-});
+    return this.store.findAll('question');
+    },
+  });
