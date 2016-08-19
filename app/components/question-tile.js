61b8885f7a3fff986ef5458934a631ca584fa447
Meaghan Jones <Guest@epicodus-90.local>
2016-08-19 09:23:17 -0700

    add ability to add a new question on the front page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  delete(question){
-    if (confirm('Are you sure you want to delete this question?')) {
-      this.sendAction('destroyQuestion');
-    }
-  }
+
 });
